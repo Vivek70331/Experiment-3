c17b44e1ab03cd650191926c9c9cc3bd132fda98
VIVEK YADAV <23bai70331@cuchd.in>
2026-02-20 16:45:52 +0530

    Fixed index.js for Vercel deployment (removed unused imports)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AppProvider } from "./context/AppContext";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <AppProvider>
-    <App />
-  </AppProvider>
-);
+root.render(<App />);
